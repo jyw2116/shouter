5ead9a29034117477b557075adf969d6c430d5c0
Jiyeon Woo <jiyeon8497@gmail.com>
2013-04-29 11:09:33 -0400

    add dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Shouter::Application.routes.draw do
-  root to: "homes#show" 
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: 'dashboards#show'
+  end
+
+  resource  :dashboard, only: [:show]
+  # or could do this... but this is the least extensible
+  get "dashboard" => "dashboards#show"
 end
